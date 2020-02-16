08af30804871d773c0599c736dff3c31cca41986
wanghg <627840236@qq.com>
2020-02-16 17:19:01 +0800

    1

---
+++ php/yiqin.php
@@ -0,0 +1,27 @@
+<?php
+@header("content-type:text/html;charset=utf8");
+@header("Access-Control-Allow-Origin:*");
+$servername = "b-xafvg3p14neoej.bch.rds.gz.baidubce.com:3306";
+$username = "b_xafvg3p14neoej";
+$password = "Q7hTI07YYEt8loSu";
+$dbname = "b_xafvg3p14neoej";
+// $data = $_POST['data']; 
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+ 
+$tmpData = strval(file_get_contents("php://input"));
+$DataArray = json_decode($tmpData, true);
+
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode($DataArray)
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
+?>
