0ba16a2c0ca06ecae9f0f146d4c9aeb942c94c2f
WANGHG\wanghg <627840236@qq.com>
2020-04-12 18:23:55 +0800

    2

--- hb2/src/services/logmanager.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request';
-
-export async function getLogList(params) {
-  const { current, pageSize } = params;
-  return request(`/cas/log/list/${current}/${pageSize}`);
-}
-
-export default getLogList;
--- hb2/src/services/wto/Statistics/OperationTypeStatisticsApi.js
+++
@@ -1,5 +0,0 @@
-import request from '@/utils/request';
-
-export async function fhwtoaccessoperate() {
-  return request(`/services/wto/fhwtoaccessoperate/list/all`);
-}
